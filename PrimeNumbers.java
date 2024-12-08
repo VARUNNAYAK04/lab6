ce1a249fc0a262eb7e21735f6ac468124e16bfc6
VARUNNAYAK04 <varoondhanavath@gmail.com>
2024-12-08 22:24:23 +0530

    hello

---
+++ PrimeNumbers.java
@@ -0,0 +1,20 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int limit = 100; // Change this to set the range
+        for (int num = 2; num <= limit; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
